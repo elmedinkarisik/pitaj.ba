1034feaf715082836d28a136c689ebb1d521a0fc
elmedinkarisik <edo.karisik1@gmail.com>
2021-02-12 21:51:05 +0100

    Added Chakra UI Popover

--- src/components/question.js
+++ src/components/question.js
@@ -1,6 +1,17 @@
 import { Box, HStack, Text } from "@chakra-ui/core";
 import React from "react";
 import VoteButtons from "./vote-buttons";
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    Button
+  } from "@chakra-ui/react"
 
 const Question = ({ question }) => {
     return (
@@ -8,6 +19,17 @@ const Question = ({ question }) => {
             <VoteButtons question = {question} />
             <Box bg="gray.100" p={4} rounded="md" w="100%">
                 <Text>{question.title}</Text>
+                <Popover>
+                <PopoverTrigger>
+                    <Button>Trigger</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader>Confirmation!</PopoverHeader>
+                    <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+                </PopoverContent>
+                </Popover>
             </Box>
         </HStack>
     );
