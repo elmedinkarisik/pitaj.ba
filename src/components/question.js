caecb2681a278c57ab79213f96e4256f0564567b
elmedinkarisik <edo.karisik1@gmail.com>
2021-02-12 21:59:10 +0100

    Reverting to the state of the project at 0db1759282205a4480d69d4afc3fbc1df078ee88

--- src/components/question.js
+++ src/components/question.js
@@ -1,17 +1,6 @@
 import { Box, HStack, Text } from "@chakra-ui/core";
 import React from "react";
 import VoteButtons from "./vote-buttons";
-import {
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverFooter,
-    PopoverArrow,
-    PopoverCloseButton,
-    Button
-  } from "@chakra-ui/react"
 
 const Question = ({ question }) => {
     return (
@@ -19,17 +8,6 @@ const Question = ({ question }) => {
             <VoteButtons question = {question} />
             <Box bg="gray.100" p={4} rounded="md" w="100%">
                 <Text>{question.title}</Text>
-                <Popover>
-                <PopoverTrigger>
-                    <Button>Trigger</Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <PopoverArrow />
-                    <PopoverCloseButton />
-                    <PopoverHeader>Confirmation!</PopoverHeader>
-                    <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-                </PopoverContent>
-                </Popover>
             </Box>
         </HStack>
     );
